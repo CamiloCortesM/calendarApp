e42c7f56b87d1385b4198a4a259b619652f94e47
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-08-23 23:55:39 -0500

    customize the event box

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,7 +3,7 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { addHours } from "date-fns";
 
-import { Navbar } from "../components/Navbar";
+import { Navbar, CalendarEvent } from "../";
 import { localizer, getMessagesES } from "../../helpers";
 
 const events = [
@@ -15,25 +15,24 @@ const events = [
     bgColor: "#fafafa",
     user: {
       _id: "123",
-      name: "Fernando",
+      name: "Camilo",
     },
   },
 ];
 
 export const CalendarPage = () => {
   const eventStyleGetter = (event, start, end, isSelected) => {
-    console.log({ event, start, end, isSelected });
 
-    const style ={
-      backgroundColor: '#347CF7',
-      borderRadius:'0px',
-      opacity:0.8,
-      color:'white'
-    }
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
 
-    return{
-      style
-    }
+    return {
+      style,
+    };
   };
   return (
     <>
@@ -47,6 +46,9 @@ export const CalendarPage = () => {
         style={{ height: "calc( 100vh - 80px )" }}
         messages={getMessagesES()}
         eventPropGetter={eventStyleGetter}
+        components={{
+          event: CalendarEvent,
+        }}
       />
     </>
   );
