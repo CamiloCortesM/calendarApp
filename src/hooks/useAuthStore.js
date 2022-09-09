dfce1160d42988c9abae7c1ea466b3b572692d5a
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-09-09 00:18:54 -0500

    maintain authentication status

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -34,10 +34,29 @@ export const useAuthStore = () => {
       localStorage.setItem("token-init-date", new Date().getTime());
       dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
-      dispatch(onLogout(error.response.data?.msg || 'Ups error en la creacion de la cuenta'));
+      dispatch(
+        onLogout(
+          error.response.data?.msg || "Ups error en la creacion de la cuenta"
+        )
+      );
       setTimeout(() => {
         dispatch(clearErrorMessage());
       }, 10);
+    }
+  };
+
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return dispatch(onLogout());
+
+    try {
+      const { data } = await calendarApi.get("/auth/renew");
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogout());
     }
   };
 
@@ -50,5 +69,6 @@ export const useAuthStore = () => {
     //* Methods
     startRegister,
     startLogin,
+    checkAuthToken,
   };
 };
