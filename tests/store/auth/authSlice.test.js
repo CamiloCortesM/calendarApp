124d49d5d8927d46d1edf2a54247172e7a8c776a
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-09-15 23:30:44 -0500

    test with reducers in authSlice

--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,8 +1,51 @@
-import { authSlice } from "../../../src/store/auth/authSlice";
-import { initialState } from "../../fixtures/authStates";
+import {
+  authSlice,
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+} from "../../../src/store/auth/authSlice";
+import {
+  authenticatedState,
+  initialState,
+  notAuthenticatedState,
+} from "../../fixtures/authStates";
+import { testUserCredentials } from "../../fixtures/testUser";
 
 describe("test in authSlice", () => {
   test("must return initialState", () => {
     expect(authSlice.getInitialState()).toEqual(initialState);
+  });
+
+  test("must make the login", () => {
+    const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+    expect(state).toEqual({
+      ...authenticatedState,
+      user: testUserCredentials,
+    });
+  });
+
+  test("must make the logout", () => {
+    const state = authSlice.reducer(authenticatedState, onLogout());
+    expect(state).toEqual(notAuthenticatedState);
+  });
+
+  test("must make the logout", () => {
+    const errorMessage = "Credencials error";
+    const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+    expect(state).toEqual({ ...notAuthenticatedState, errorMessage });
+  });
+
+  test("must clear the errorMessage", () => {
+    const errorMessage = "Credencials error";
+    const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+    const newState = authSlice.reducer(state, clearErrorMessage());
+    expect(newState.errorMessage).toBe(undefined);
+  });
+
+  test("should call checking and return the initialstate", () => {
+    let state = authSlice.reducer(authenticatedState, onLogout());
+    state = authSlice.reducer(state, onChecking());
+    expect(state).toEqual(initialState);
   });
 });
