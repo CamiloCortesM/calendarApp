38116c70bde2888eca07d03d5472e36ffffcd197
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-09-16 23:24:39 -0500

    test in useUiStore

---
+++ tests/hooks/useUiStore.test.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { renderHook } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { useUiStore } from "../../src/hooks";
+import { uiSlice } from "../../src/store";
+
+const getMockStore = (initialState) => {
+  return configureStore({
+    reducer: {
+      ui: uiSlice.reducer,
+    },
+    preloadedState: {
+      ui: { ...initialState },
+    },
+  });
+};
+
+describe("test in useUiStore", () => {
+  test("must return initialState", () => {
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+
+    expect(result.current).toEqual({
+      isDateModalOpen: false,
+      openDateModal: expect.any(Function),
+      closeDateModal: expect.any(Function),
+      toggleDateModal: expect.any(Function),
+    });
+  });
+});
