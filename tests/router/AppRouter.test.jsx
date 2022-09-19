8851d882feb178a430818df4880a7ca29a3f4f0e
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-09-18 23:54:45 -0500

    test in Approute

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { useAuthStore } from "../../src/hooks/useAuthStore";
+import { AppRouter } from "../../src/router/AppRouter";
+
+jest.mock("../../src/hooks/useAuthStore");
+
+describe("test in AppRouter", () => {
+  const mockcheckAuthToken = jest.fn();
+  beforeEach(() => jest.clearAllMocks());
+
+  test("must display the loading screen and call checkAuthToken", () => {
+    useAuthStore.mockReturnValue({
+      status: "checking",
+      checkAuthToken: mockcheckAuthToken,
+    });
+    render(<AppRouter />);
+    expect(screen.getByText("Cargando...")).toBeTruthy();
+    expect(mockcheckAuthToken).toHaveBeenCalledWith();
+  });
+});
