710736cca4b03c8508148f2a0acadbc73811d654
CamiloCortesM <juan.cortes05@uptc.edu.co>
2022-09-18 23:58:16 -0500

    test in Approute with not-authenticated with login

--- tests/router/AppRouter.test.jsx
+++ tests/router/AppRouter.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { useAuthStore } from "../../src/hooks/useAuthStore";
 import { AppRouter } from "../../src/router/AppRouter";
 
@@ -16,5 +17,20 @@ describe("test in AppRouter", () => {
     render(<AppRouter />);
     expect(screen.getByText("Cargando...")).toBeTruthy();
     expect(mockcheckAuthToken).toHaveBeenCalledWith();
+  });
+
+  test("must display the login if there is not-authenticated", () => {
+    useAuthStore.mockReturnValue({
+      status: "not-authenticated",
+      checkAuthToken: mockcheckAuthToken,
+    });
+    render(
+      <MemoryRouter>
+        <AppRouter />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Ingreso")).toBeTruthy();
+    expect(screen.getByText("Registro")).toBeTruthy();
   });
 });
